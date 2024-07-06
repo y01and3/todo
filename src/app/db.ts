89f491913a7a6d91fa2d78d26229baa4db2810d2
y01and3 <1791280644@qq.com>
2024-07-06 17:41:40 +0800

    refactor(todo db): remove undefined properties in db

--- src/app/db.ts
+++ src/app/db.ts
@@ -2,12 +2,18 @@ import type { EntityTable } from "dexie"
 import Dexie from "dexie"
 import type { Todo } from "../features/todo/todo.type"
 
-export interface TodoItem extends Todo {
+interface TodoDb extends Todo {
   order: number
 }
 
+type RemoveUndefinedProperties<T> = {
+  [K in keyof T]: T[K] extends undefined ? undefined : T[K];
+};
+
+type TodoDbItem = RemoveUndefinedProperties<TodoDb>;
+
 export const db = new Dexie("TodosDatabase") as Dexie & {
-  todos: EntityTable<TodoItem, "id">
+  todos: EntityTable<TodoDbItem, "id">
 }
 
 db.version(1).stores({ todos: "++id, title, completed, priority, &order" })
--- src/features/todo/testDb.ts
+++ src/features/todo/testDb.ts
@@ -4,16 +4,22 @@ import type { Todo } from "./todo.type"
 import { indexedDB, IDBKeyRange } from "fake-indexeddb"
 
 type Picked = "title" | "createdAt" | "changedAt"
-export interface TodoItem extends Pick<Todo, Picked> {
+interface TodoDb extends Pick<Todo, Picked> {
   id?: number
   order: number
 }
+
+type RemoveUndefinedProperties<T> = {
+  [K in keyof T]: T[K] extends undefined ? undefined : T[K]
+}
+
+type TodoDbItem = RemoveUndefinedProperties<TodoDb>
 
 export const db = new Dexie("TodoTestDatabase", {
   indexedDB,
   IDBKeyRange,
 }) as Dexie & {
-  todos: EntityTable<TodoItem, "id">
+  todos: EntityTable<TodoDbItem, "id">
 }
 
 db.version(1).stores({ todos: "++id, title, &order" })
